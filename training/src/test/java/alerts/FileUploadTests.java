116d62be17e47d46e5bb49acb167343b10c2441a
Justin Tung <justin.tung@live.ca>
2021-06-21 18:35:58 -0400

    bump htmlunit-driver to 2.50.0, test verification

--- training/src/test/java/alerts/FileUploadTests.java
+++ training/src/test/java/alerts/FileUploadTests.java
@@ -9,7 +9,7 @@ public class FileUploadTests extends BaseTests {
     @Test
     public void testFileUpload() {
         var fileUploadPage = homePage.clickFileUpload();
-        fileUploadPage.uploadFile("G:/u/My Documents Non-Essentials/Code/selenium-java-tests/LICENSE");
+        fileUploadPage.uploadFile("D:/usr/Code/selenium-java-tests/LICENSE");
 
         Assert.assertEquals(fileUploadPage.getUploadedFiles(), "LICENSE", "Uploaded file not correct");
     }
